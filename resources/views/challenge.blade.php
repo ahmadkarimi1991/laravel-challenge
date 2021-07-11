86e009b95ef36aefd3f9eadd79e76d7c03ff1a34
Ahmad <ahmadkarimi1991@gmail.com>
2021-07-12 00:34:55 +0430

    Create meetings section

--- resources/views/challenge.blade.php
+++ resources/views/challenge.blade.php
@@ -92,6 +92,48 @@
         </div>
     </section>
 
+    <section class="challenge-meetings">
+        <div class="container">
+            <h2 class="challenge-meetings__title">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+                    <g id="Group_13293" data-name="Group 13293" transform="translate(-1482 -3883)">
+                        <g id="Group_13275" data-name="Group 13275" transform="translate(908 2987)">
+                            <rect id="Rectangle_1609" data-name="Rectangle 1609" width="48" height="48" rx="12" transform="translate(574 896)" fill="#c9d9ea" opacity="0.36"></rect>
+                            <rect id="Rectangle_1608" data-name="Rectangle 1608" width="38" height="38" rx="8" transform="translate(579 901)" fill="#20c5ba"></rect>
+                        </g>
+                        <g id="users_3_" data-name="users (3)" transform="translate(1494 3895)">
+                            <path id="Path_133688" data-name="Path 133688" d="M17,21V19a4,4,0,0,0-4-4H5a4,4,0,0,0-4,4v2" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                            <circle id="Ellipse_1351" data-name="Ellipse 1351" cx="4" cy="4" r="4" transform="translate(5 3)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
+                            <path id="Path_133689" data-name="Path 133689" d="M23,21V19a4,4,0,0,0-3-3.87" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                            <path id="Path_133690" data-name="Path 133690" d="M16,3.13a4,4,0,0,1,0,7.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                        </g>
+                    </g>
+                </svg>
+
+                <span>ساختار جلسات</span>
+            </h2>
+            <div class="challenge-meetings__text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.</div>
+        </div>
+    </section>
+
+    <section class="challenge-form">
+        <div class="container">
+            <div>Form</div>
+        </div>
+    </section>
+
+    <section class="challenge-market">
+        <div class="container">
+            <div>Market</div>
+        </div>
+    </section>
+
+    <section class="challenge-success">
+        <div class="container">
+            <div>Success</div>
+        </div>
+    </section>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
