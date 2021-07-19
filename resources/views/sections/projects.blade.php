1c006fdff7d9d0a28f5f6814eea8dc75fe4392d4
Ahmad <ahmadkarimi1991@gmail.com>
2021-07-19 15:20:16 +0430

    Fix a class name in projects section

--- resources/views/sections/projects.blade.php
+++ resources/views/sections/projects.blade.php
@@ -3,7 +3,7 @@
         <h2 class="challenge-projects__title">فهرست پروژه‌ها</h2>
         <div class="challenge-projects__text">با فهرست پروژه‌های در جریان ابر آروان بیش‌تر آشنا شوید</div>
         <div class="challenge-projects__tabs">
-            <div class="challenge-projects__tabs-wrapper">
+            <div class="challenge-projects__tabs-wrap">
                 <a href="#tab-1" class="challenge-projects__tab">پروژه‌های فعال</a>
                 <a href="#tab-2" class="challenge-projects__tab">در حال ارزیابی</a>
                 <a href="#tab-3" class="challenge-projects__tab">پروژه‌های اجرا شده</a>
