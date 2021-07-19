f52110737121ae2668487c2d49b4272f88416388
Ahmad <ahmadkarimi1991@gmail.com>
2021-07-20 02:01:04 +0430

    Add logos to success section

--- resources/views/sections/success.blade.php
+++ resources/views/sections/success.blade.php
@@ -1,5 +1,38 @@
 <section class="challenge-success">
     <div class="container">
         <h2 class="challenge-success__title">پروژه‌های موفق</h2>
+        <div class="challenge-success__wrap">
+            <div class="challenge-success__item">
+                <img src="/images/s1.svg" alt="Success Item 1" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s2.svg" alt="Success Item 2" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s3.svg" alt="Success Item 3" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s4.svg" alt="Success Item 4" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s1.svg" alt="Success Item 1" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s2.svg" alt="Success Item 2" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s3.svg" alt="Success Item 3" class="challenge-success__item-img">
+            </div>
+
+            <div class="challenge-success__item">
+                <img src="/images/s4.svg" alt="Success Item 4" class="challenge-success__item-img">
+            </div>
+        </div>
     </div>
 </section>
