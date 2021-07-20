121f5cd7c874961c69099e365fe38b424890dc4e
Ahmad <ahmadkarimi1991@gmail.com>
2021-07-20 22:21:17 +0430

    Add some content to projects

--- resources/views/sections/projects.blade.php
+++ resources/views/sections/projects.blade.php
@@ -56,7 +56,85 @@
                                 </svg>
                             </div>
                         </div>
-                        <div class="challenge-projects__item-content">Content</div>
+                        <div class="challenge-projects__item-content">
+                            <div class="challenge-projects__projects-wrap">
+                                <div class="challenge-projects__project-content">
+                                    <div>
+                                        <div class="challenge-projects__project-img">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="49.224" height="49.224" viewBox="0 0 49.224 49.224">
+                                                <g id="Group_13053" data-name="Group 13053" transform="translate(-1214 -2161)">
+                                                    <path id="Path_131059" data-name="Path 131059" d="M12.306,0H36.918A12.306,12.306,0,0,1,49.224,12.306V36.918A12.306,12.306,0,0,1,36.918,49.224H12.306A12.306,12.306,0,0,1,0,36.918V12.306A12.306,12.306,0,0,1,12.306,0Z" transform="translate(1214 2161)" fill="#e7ecf4"></path>
+                                                    <rect id="Rectangle_3870" data-name="Rectangle 3870" width="39" height="39" rx="8" transform="translate(1219 2166)" fill="#20c5ba"></rect>
+                                                    <g id="file-plus_2_" data-name="file-plus (2)" transform="translate(1227 2174)">
+                                                        <path id="Path_131063" data-name="Path 131063" d="M14,2H6A2,2,0,0,0,4,4V20a2,2,0,0,0,2,2H18a2,2,0,0,0,2-2V8Z" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                                                        <path id="Path_131064" data-name="Path 131064" d="M14,2V8h6" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                                                        <line id="Line_951" data-name="Line 951" y1="6" transform="translate(12 12)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></line>
+                                                        <line id="Line_952" data-name="Line 952" x2="6" transform="translate(9 15)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></line>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div class="challenge-projects__project-title">درخواست دهنده</div>
+                                        <div class="challenge-projects__project-subtitle">تیم CDN</div>
+                                    </div>
+                                </div>
+                                <div class="challenge-projects__project-content">
+                                    <div>
+                                        <div class="challenge-projects__project-img">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="49.224" height="49.224" viewBox="0 0 49.224 49.224">
+                                                <g id="Group_13054" data-name="Group 13054" transform="translate(-1028 -2161)">
+                                                    <path id="Path_131060" data-name="Path 131060" d="M12.306,0H36.918A12.306,12.306,0,0,1,49.224,12.306V36.918A12.306,12.306,0,0,1,36.918,49.224H12.306A12.306,12.306,0,0,1,0,36.918V12.306A12.306,12.306,0,0,1,12.306,0Z" transform="translate(1028 2161)" fill="#e7ecf4"></path>
+                                                    <rect id="Rectangle_3871" data-name="Rectangle 3871" width="39" height="39" rx="8" transform="translate(1033 2166)" fill="#20c5ba"></rect>
+                                                    <g id="arrow-up-circle" transform="translate(1041 2174)">
+                                                        <circle id="Ellipse_1256" data-name="Ellipse 1256" cx="10" cy="10" r="10" transform="translate(2 2)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
+                                                        <path id="Path_131067" data-name="Path 131067" d="M16,12,12,8,8,12" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                                                        <line id="Line_953" data-name="Line 953" y1="8" transform="translate(12 8)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></line>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div class="challenge-projects__project-title">روش اجرا و ارائه</div>
+                                        <div class="challenge-projects__project-subtitle">Opensource/Github</div>
+                                    </div>
+                                </div>
+                                <div class="challenge-projects__project-content">
+                                    <div>
+                                        <div class="challenge-projects__project-img">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="49.224" height="49.224" viewBox="0 0 49.224 49.224">
+                                                <g id="Group_13055" data-name="Group 13055" transform="translate(-842 -2161)">
+                                                    <path id="Path_131061" data-name="Path 131061" d="M12.306,0H36.918A12.306,12.306,0,0,1,49.224,12.306V36.918A12.306,12.306,0,0,1,36.918,49.224H12.306A12.306,12.306,0,0,1,0,36.918V12.306A12.306,12.306,0,0,1,12.306,0Z" transform="translate(842 2161)" fill="#e7ecf4"></path>
+                                                    <rect id="Rectangle_3872" data-name="Rectangle 3872" width="39" height="39" rx="8" transform="translate(847 2166)" fill="#20c5ba"></rect>
+                                                    <g id="code" transform="translate(855 2174)">
+                                                        <path id="Path_131065" data-name="Path 131065" d="M16,18l6-6L16,6" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                                                        <path id="Path_131066" data-name="Path 131066" d="M8,6,2,12l6,6" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div class="challenge-projects__project-title">زبان پروژه</div>
+                                        <div class="challenge-projects__project-subtitle">Python</div>
+                                    </div>
+                                </div>
+                                <div class="challenge-projects__project-content">
+                                    <div>
+                                        <div class="challenge-projects__project-img">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="49.224" height="49.224" viewBox="0 0 49.224 49.224">
+                                                <g id="Group_13056" data-name="Group 13056" transform="translate(-656 -2161)">
+                                                    <path id="Path_131062" data-name="Path 131062" d="M12.306,0H36.918A12.306,12.306,0,0,1,49.224,12.306V36.918A12.306,12.306,0,0,1,36.918,49.224H12.306A12.306,12.306,0,0,1,0,36.918V12.306A12.306,12.306,0,0,1,12.306,0Z" transform="translate(656 2161)" fill="#e7ecf4"></path>
+                                                    <rect id="Rectangle_3873" data-name="Rectangle 3873" width="39" height="39" rx="8" transform="translate(661 2166)" fill="#20c5ba"></rect>
+                                                    <g id="dollar-sign_4_" data-name="dollar-sign (4)" transform="translate(669 2174)">
+                                                        <line id="Line_954" data-name="Line 954" y2="22" transform="translate(12 1)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></line>
+                                                        <path id="Path_131068" data-name="Path 131068" d="M17,5H9.5a3.5,3.5,0,0,0,0,7h5a3.5,3.5,0,1,1,0,7H6" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div class="challenge-projects__project-title">مبلغ پروژه</div>
+                                        <div class="challenge-projects__project-subtitle">10,000,000 ریال</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="challenge-projects__separator"></div>
