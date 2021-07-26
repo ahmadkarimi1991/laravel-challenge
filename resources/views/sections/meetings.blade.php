2d3d226cbd19f0e660feef86bcc848ce496214a1
Ahmad <ahmadkarimi1991@gmail.com>
2021-07-26 22:21:53 +0430

    Create box partial for projects and meetings

--- resources/views/sections/meetings.blade.php
+++ resources/views/sections/meetings.blade.php
@@ -32,16 +32,17 @@
 
             <div class="challenge-meetings__boxes">
                 <div class="challenge-meetings__box challenge-meetings__box--active">
-                    <div class="challenge-meetings__box-title">جلسات استراتژی</div>
-                    <p class="challenge-meetings__box-text">هر یک سال یک بار مجموعه جلساتی تحت عنوان جلسات برنامه ریزی استراتژیک برگزار می‌شود. براساس این جلسات تیم راهبری داستان سال را می‌نویسد و پس از بحث و تبادل نظر OKR یک‌ساله‌ی شرکت نگارش می‌شود.</p>
-                    <p class="challenge-meetings__box-text">پس از گذشت ۶ ماهه مجموعه جلساتی در راستای بازنگری و یا اصلاح استراتژی و یا OKR سازمان متناسب با رخدادهای ۶ ماه گذشته برگزار می‌شود.</p>
-                    <div class="challenge-meetings__box-title challenge-meetings__box-title--secondary">جلسات استراتژی</div>
-                    <p class="challenge-meetings__box-text">تمامی اعضای شرکت می‌توانند در این جلسات حضور پیدا کنند. حضور راهبرهای هر تیم در این جلسات اجباری است.</p>
+                    @include('sections.meetings.box')
                 </div>
-
-                <div class="challenge-meetings__box">2</div>
-                <div class="challenge-meetings__box">3</div>
-                <div class="challenge-meetings__box">4</div>
+                <div class="challenge-meetings__box">
+                    @include('sections.meetings.box')
+                </div>
+                <div class="challenge-meetings__box">
+                    @include('sections.meetings.box')
+                </div>
+                <div class="challenge-meetings__box">
+                    @include('sections.meetings.box')
+                </div>
             </div>
         </div>
     </div>
---
+++ resources/views/sections/meetings/box.blade.php
@@ -0,0 +1,5 @@
+<div class="challenge-meetings__box-title">جلسات استراتژی</div>
+                    <p class="challenge-meetings__box-text">هر یک سال یک بار مجموعه جلساتی تحت عنوان جلسات برنامه ریزی استراتژیک برگزار می‌شود. براساس این جلسات تیم راهبری داستان سال را می‌نویسد و پس از بحث و تبادل نظر OKR یک‌ساله‌ی شرکت نگارش می‌شود.</p>
+                    <p class="challenge-meetings__box-text">پس از گذشت ۶ ماهه مجموعه جلساتی در راستای بازنگری و یا اصلاح استراتژی و یا OKR سازمان متناسب با رخدادهای ۶ ماه گذشته برگزار می‌شود.</p>
+                    <div class="challenge-meetings__box-title challenge-meetings__box-title--secondary">جلسات استراتژی</div>
+                    <p class="challenge-meetings__box-text">تمامی اعضای شرکت می‌توانند در این جلسات حضور پیدا کنند. حضور راهبرهای هر تیم در این جلسات اجباری است.</p>
