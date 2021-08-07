865f816fb6707f9e1569735c4a068ce76c3e6db3
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-08-07 21:32:19 +0430

    Add chevron svg to buttons

--- resources/views/sections/success.blade.php
+++ resources/views/sections/success.blade.php
@@ -2,8 +2,16 @@
     <div class="container">
         <h2 class="challenge-success__title">پروژه‌های موفق</h2>
         <div class="challenge-success__wrap">
-            <div class="challenge-success__chevron challenge-success__chevron--right">&#139;</div>
-            <div class="challenge-success__chevron challenge-success__chevron--left challenge-success__chevron--hide">&#155;</div>
+            <div class="challenge-success__chevron challenge-success__chevron--right">
+                <svg width="8" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 11L6 6L1 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </div>
+            <div class="challenge-success__chevron challenge-success__chevron--left challenge-success__chevron--hide">
+                <svg width="8" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 11L6 6L1 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </div>
 
             <div class="challenge-success__item">
                 <img src="/images/s1.svg" alt="Success Item 1" class="challenge-success__item-img">
